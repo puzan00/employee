14216e3af3077f49a274c8bb16db499263729012
SatkarNiraula <satkarniraula69@gmail.com>
2023-03-19 13:46:59 +0545

    corrected

--- emp_app/views.py
+++ emp_app/views.py
@@ -1,24 +1,36 @@
 from django.shortcuts import render,HttpResponse,redirect
-from emp_app.models import Employee
+from emp_app.models import Employee, Department, Role
+from .forms import EmployeeForm
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 def view_all(request):
     emps=Employee.objects.all()
     return render(request, 'view_all.html',{'emps':emps})
+
 def add(request):
     if request.method=='POST':
         fn=request.POST['first_name']
         ln=request.POST['last_name']
         salary=request.POST['salary']
-        dep=request.POST['dept']
-        rol=request.POST['role']
+        dep = Department.objects.get(id=int(request.POST['dept']))
+        # dep=request.POST['dept']
+        # rol=request.POST['role']
+        rol = Role.objects.get(id=int(request.POST['role']))
         bonus=request.POST['bonus']
         phno=request.POST['phone']
-        Employee.objects.create(first_name=fn, last_name=ln, salary=salary, dept=dep, role=rol, bonus=bonus, phone=phno)
-        Employee.save() 
+        
+        user = Employee(first_name=fn, last_name=ln, salary=salary, dept=dep, role=rol, bonus=bonus, phone=phno)
+        user.save() 
+        return redirect('/add/')
     elif request.method=="GET":
-        return render(request, 'add.html')
+        departments = Department.objects.all()
+        roles = Role.objects.all()
+        context = {
+            "departments": departments,
+            "roles" :roles
+        }
+        return render(request, 'add.html', context)
     else:
         return HttpResponse("Exception occurred could not add employee")
 
